6767b6a5e06a4781da36a0fc84f844a3cb997e1a
Austin Jess <ajess33@gmail.com>
2019-04-25 10:28:04 -0500

    location route gets data

--- schema.sql
+++ schema.sql
@@ -4,18 +4,18 @@ DROP TABLE IF EXISTS locations;
 -- each specific location will be a row
 
 -- columns
-CREATE TABLE locations {
+CREATE TABLE locations (
   id SERIAL PRIMARY KEY,
   search_query VARCHAR(255),
   formatted_query VARCHAR(255),
   latitude NUMERIC(10, 7),
   longitude NUMERIC(10, 7)
-}
+);
 
-CREATE TABLE weather {
+CREATE TABLE weather (
   id SERIAL PRIMARY KEY,
   forecast VARCHAR(255),
   time VARCHAR(255),
   location_id INTEGER NOT NULL,
   FOREIGN KEY(location_id) REFERENCES locations (id)
-}
+);
