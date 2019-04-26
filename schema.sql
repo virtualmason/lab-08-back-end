4ab97bcc3502b2ec5a729d7bedc43771e5e42d55
Floyd Orr <floydcanhelp@gmail.com>
2019-04-26 11:17:36 -0500

    get data from server

--- schema.sql
+++ schema.sql
@@ -1,5 +1,9 @@
 DROP TABLE IF EXISTS weathers;
 DROP TABLE IF EXISTS locations;
+DROP TABLE IF EXISTS movies;
+
+
+
 
 -- each specific location will be a row
 
@@ -19,3 +23,17 @@ CREATE TABLE weathers (
   location_id INTEGER NOT NULL,
   FOREIGN KEY(location_id) REFERENCES locations (id)
 );
+
+CREATE TABLE movies ( 
+  id SERIAL PRIMARY KEY, 
+  title VARCHAR(255), 
+  overview VARCHAR(1000), 
+  average_votes NUMERIC(4,2), 
+  total_votes INTEGER, 
+  image_url VARCHAR(255), 
+  popularity NUMERIC(6,4), 
+  released_on CHAR(10),
+  created_at BIGINT,
+  location_id INTEGER NOT NULL REFERENCES locations(id) 
+);
+
